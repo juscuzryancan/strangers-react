f18228fdd20c24882ce9a1c61f158910fd0e300e
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-05-13 14:30:51 -0700

    puts padding around the welcome message

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -24,8 +24,6 @@ const Sidebar = ({
             {!token  && <Button onClick={() => history.push('/register')}>Register</Button>}
             {token && <h3>Welcome, {user.username}</h3>}
             {token && <Button onClick={() => history.push('/createpost')}>Create A Post</Button>}
-            {token && <Button onClick={() => console.log()}>All Posts</Button>}
-            {token && <Button onClick={() => console.log()}>My Posts</Button>}
             {token && <Button onClick={() => history.push('/messages')}>Messages</Button>}
             {token && <Button onClick={handleSignOut}>Sign Out</Button>}
         </nav>
